f7e4b863ac2df5a88200493e116be0b6f838fc17
ouxy <ouxy@cug2313.com>
2018-11-26 17:17:48 +0800

    change compileOnly

--- app/src/main/java/com/oxy/prioritylinearlayout/MainActivity.java
+++ app/src/main/java/com/oxy/prioritylinearlayout/MainActivity.java
@@ -2,7 +2,9 @@ package com.oxy.prioritylinearlayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,18 +15,50 @@ import java.util.List;
 public class MainActivity extends Activity {
     ArrayAdapter adapter;
     private List<String> list = new ArrayList<>();
+    private View layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listView = findViewById(R.id.listView);
+        layout = findViewById(R.id.layout);
         list.add("item 0");
         list.add("item 1");
         list.add("item 2");
         list.add("item 3");
         list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
+        list.add("item 4");
         listView.setAdapter(adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,
                 list));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("oxy","layout.getMeasuredHeight():"+layout.getMeasuredHeight());
+                Log.d("oxy","layout.getHeight():"+layout.getHeight());
+            }
+        });
     }
 
     public void onAddChild(View v){
